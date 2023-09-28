f6a8a09f7047df0cafbdb7335656a67d69d78418
Harsh Rai <harshpayal3252@gmail.com>
2023-09-28 17:43:26 +0530

    edit functionality

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const resumeForm = document.getElementById("resumeForm");
     const resumeList = document.getElementById("resumeList");
 
+
     resumeForm.addEventListener("submit", (e) => {
         e.preventDefault();
         const name = document.getElementById("name").value;
@@ -10,7 +11,9 @@ document.addEventListener("DOMContentLoaded", () => {
         const gender = document.getElementById("gender").value;
         const project = document.getElementById("project").value;
         const dob = document.getElementById("dob").value;
-
+                                                                                                                                    
+        const skillsSelect = document.getElementById("skills");
+        const selectedSkills = Array.from(skillsSelect.selectedOptions).map(option => option.value);
         const resume = {
             name,
             age,
@@ -18,24 +21,22 @@ document.addEventListener("DOMContentLoaded", () => {
             gender,
             project,
             dob,
+            skills: selectedSkills,
         };
 
         const editIndex = parseInt(resumeForm.dataset.editIndex);
         if (editIndex >= 0) {
-            // Editing an existing resume
             const resumes = JSON.parse(localStorage.getItem("resumes")) || [];
-            resumes[editIndex] = resume; // Use "resume" instead of "editedResume"
+            resumes[editIndex] = resume;
             localStorage.setItem("resumes", JSON.stringify(resumes));
-            resumeForm.dataset.editIndex = -1; // Reset the edit index
+            resumeForm.dataset.editIndex = -1;
         } else {
-            // Creating a new resume
             saveResume(resume);
         }
-
         resumeForm.reset();
         renderResumeList();
     });
-       //for saving and pushing in array
+
     function saveResume(resume) {
         let resumes = JSON.parse(localStorage.getItem("resumes")) || [];
         resumes.push(resume);
@@ -45,19 +46,25 @@ document.addEventListener("DOMContentLoaded", () => {
 
     function renderResumeList() {
         resumeList.innerHTML = "";
-
         const resumes = JSON.parse(localStorage.getItem("resumes")) || [];
-
         for (let i = 0; i < resumes.length; i++) {
             const resume = resumes[i];
             const listItem = document.createElement("li");
-            listItem.innerHTML = `
-                Name: ${resume.name}
-                <button class="view" data-index="${i}">View Resume</button>
-                <button class="delete" data-index="${i}">Delete</button>
-                <button class="edit" data-index="${i}">Edit</button>
-            `;
-
+          listItem.innerHTML = `
+    <div class="resume-container">
+        <div class="resume-info">
+            <p>Name: ${resume.name}</p>
+            <p>Age: ${resume.age}</p>
+            <p>Experience: ${resume.experience}</p>
+            <p>DOB: ${resume.dob}</p>
+        </div>
+        <div class="resume-actions">
+            <button class="view" data-index="${i}">View Resume</button>
+            <button class="delete" data-index="${i}">Delete</button>
+            <button class="edit" data-index="${i}">Edit</button>
+        </div>
+    </div>
+`;
             resumeList.appendChild(listItem);
         }
     }
@@ -75,24 +82,23 @@ document.addEventListener("DOMContentLoaded", () => {
             const index = e.target.getAttribute("data-index");
             const selectedResume = JSON.parse(localStorage.getItem("resumes"))[index];
         
-            // Populate the form fields with the selected resume's data
+
             document.getElementById("name").value = selectedResume.name;
             document.getElementById("age").value = selectedResume.age;
         
-            // Populate the radio button for experience
+         
             const experienceRadios = document.querySelectorAll('input[name="experience"]');
             for (const radio of experienceRadios) {
                 if (radio.value === selectedResume.experience) {
                     radio.checked = true;
-                    break; // Once found, exit the loop
+                    break; 
                 }
             }
-        
+
             document.getElementById("gender").value = selectedResume.gender;
             document.getElementById("project").value = selectedResume.project;
             document.getElementById("dob").value = selectedResume.dob;
         
-            // Store the index of the resume being edited for later use
             resumeForm.dataset.editIndex = index;
         }
         
@@ -122,52 +128,50 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
 
-
-
-
-
-
-
-//resume.html
 const urlParams = new URLSearchParams(window.location.search);
 const resumeData = urlParams.get("resumeData");
 
 if (resumeData) {
-
     const resume = JSON.parse(resumeData);
     console.log(resume);
     const resumeDetails = document.getElementById("resumeDetails");
     resumeDetails.innerHTML = `
     <div class="resume-container">
-    <h1>Resume</h1>
-    <div class="resume-section">
-        <div class="resume-label">Name:</div>
-        <div class="resume-data">${resume.name}</div>
+        <h1>Resume</h1>
+        <div class="resume-section">
+            <div class="resume-label">Name:</div>
+            <div class="resume-data">${resume.name}</div>
+        </div>
+        <div class="resume-section">
+            <div class="resume-label">Age:</div>
+            <div class="resume-data">${resume.age}</div>
+        </div>
+        <div class="resume-section">
+            <div class="resume-label">Experience:</div>
+            <div class="resume-data">${resume.experience}</div>
+        </div>
+        <div class="resume-section">
+            <div class="resume-label">Gender:</div>
+            <div class="resume-data">${resume.gender}</div>
+        </div>
+        <div class="resume-section">
+            <div class="resume-label">Project:</div>
+            <div class="resume-data">${resume.project}</div>
+        </div>
+        <div class="resume-section">
+            <div class="resume-label">Date of Birth:</div>
+            <div class="resume-data">${resume.dob}</div>
+        </div>
+        <div class="resume-section">
+            <div class="resume-label">Skills:</div>
+            <div class="resume-data">
+                <ul>
+                    ${resume.skills.map(skill => `<li>${skill}</li>`).join('')}
+                </ul>
+            </div>
+        </div>
     </div>
-    <div class="resume-section">
-        <div class="resume-label">Age:</div>
-        <div class="resume-data">${resume.age}</div>
-    </div>
-    <div class="resume-section">
-        <div class="resume-label">Experience:</div>
-        <div class="resume-data">${resume.experience}</div>
-    </div>
-    <div class="resume-section">
-        <div class="resume-label">Gender:</div>
-        <div class="resume-data">${resume.gender}</div>
-    </div>
-    <div class="resume-section">
-        <div class="resume-label">Project:</div>
-        <div class="resume-data">${resume.project}</div>
-    </div>
-    <div class="resume-section">
-        <div class="resume-label">Date of Birth:</div>
-        <div class="resume-data">${resume.dob}</div>
-    </div>
-</div>
-        
     `;
-
 } else {
     console.error("Resume data not found in query parameter.");
 }
